21db8148136a58187aa0330b8bb8f17ed264ae40
Luis Cabaceira <luis.cabaceira@alfresco.com>
2014-08-11 22:52:42 +0100

    including workdesk module jar extension

---
+++ amps/psg-workdesk-extension/src/main/java/org/alfresco/psgextension/DemoExtWdJar.java
@@ -0,0 +1,13 @@
+package  org.alfresco.psgextension;
+
+public class DemoExtWdJar
+{
+    public void init()
+    {
+        System.out.println("SDK Demo Workdesk Jar class has been loaded");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("SDK Demo - Workdesk Jar Hello");
+    }
+}
